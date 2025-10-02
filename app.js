21fda37969baf6b73975bb2b1293021b094af270
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-10-01 21:19:02 -0500

    code from class

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 console.log("app.js loaded");
 
-function invest() {
-  // TODO - your investment calculation!
+function invest(principal, rate, time) {
+  return principal * (1 + rate) ** time;
 }
-
-console.log(invest());
 
 /*************************************
 Ignore what is below for now...     */
 
 function displayInvestment() {
-  const investment = invest();
-  alert("The investment will be worth $" + investment);
+  const principal = getPrincipal();
+  if (principal === null) {
+    alert("Your principal is empty!");
+    return;
+  }
+
+  const rate = getReturnDecimal();
+  if (rate === null) {
+    alert("Your rate is empty!");
+  }
+  const time = getNumOfYears();
+  if (time === null) {
+    alert("Your time is empty!");
+  }
+  const investment = invest(principal, rate, time);
+  if (investment <= 0) {
+    // do something
+  } else {
+    alert("The investment will be worth $" + investment);
+  }
 }
